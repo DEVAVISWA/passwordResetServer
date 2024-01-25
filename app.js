ba710ee9e62c0e2278af31212f0218196296d33c
DEVAVISWA G <devaviswa15@gmail.com>
2024-01-25 15:34:57 +0530

    Login BE ready no boubts

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const userRouter = require("./Controller/users");
-const User = require("./Models/user");
-// const loginRouter = require("./Controller/login");
-const bcrypt= require('bcrypt')
-const jwt= require('jsonwebtoken');
-const { JWT_SECRET } = require("./Utils/config");
+const loginRouter = require("./Controller/login");
 
 app.use(cors());
 app.use(express.json());
@@ -15,25 +11,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Password Reset</h1>");
 });
 app.use('/users', userRouter)
-// app.use('/login', loginRouter)
-app.post('/login' , async(req,res)=> {
-  const {Email,Password} = req.body
-  const user= await User.findOne({Email}) 
-  if(!user){
-      return res.status(401).json({message:"user doesnt exist"})
-  }
-  const isAuthenticated=  bcrypt.compare(Password,user.passwordHash)
-  if(!isAuthenticated){
-      return res.status(400).json({message:'password is wrong'})
-  }
+app.use('/login', loginRouter)
 
-  const payload={
-      name:user.Name,
-      email: user.Email,
-      id:user._id
-  }
-  const token= jwt.sign(payload, JWT_SECRET,{expiresIn: '1h'})
-  res.status(200).json({token,Name:user.Name,Email: user.Email})
-})
 
 module.exports = app;
